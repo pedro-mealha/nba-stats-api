a6f81ad7d13d7a86e7b26c2da7b500a02cbba09f
Pedro Mealha <pedromealha@hotmail.com>
2025-06-01 22:05:19 +0100

    remove staging

--- configs/terraform/main.tf
+++ configs/terraform/main.tf
@@ -4,17 +4,20 @@ locals {
 }
 
 resource "fly_app" "nba_stats_api" {
-  name = local.app_name
-  org  = "personal"
+  count = terraform.workspace == "prod" ? 1 : 0
+  name  = local.app_name
+  org   = "personal"
 }
 
 resource "fly_ip" "nba_stats_api_ip_v6" {
+  count      = terraform.workspace == "prod" ? 1 : 0
   app        = local.app_name
   type       = "v6"
   depends_on = [fly_app.nba_stats_api]
 }
 
 resource "fly_cert" "nba_stats_api_cert" {
+  count      = terraform.workspace == "prod" ? 1 : 0
   app        = local.app_name
   hostname   = "${local.subdomain}.api.pedromealha.dev"
   depends_on = [fly_app.nba_stats_api]
